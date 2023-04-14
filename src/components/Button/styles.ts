5044898cd2db0cbf80a3e1dec6c7034bb5179548
Guilherme <gui.wustro@gmail.com>
2023-04-14 20:04:39 -0300

    fix(styles): button responsivity

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -19,6 +19,7 @@ export const ButtonStyle = styled.button<IButtonStyleProps>`
 		props.filterType &&
 		css`
 			background-color: ${(props) => props.theme.colors.purple300};
+			width: auto;
 			:hover {
 				background-color: ${(props) => props.theme.colors.purple200};
 			}
