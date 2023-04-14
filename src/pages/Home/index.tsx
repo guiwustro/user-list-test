ca71c97199c41cda312953dc7748eb0acb81b28b
Guilherme <gui.wustro@gmail.com>
2023-04-13 22:43:14 -0300

    feat: add user modal

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,14 @@
-import UserList from "../../components/UserList";
+import UserList from "../../components/UserList/UserList";
+import UserModal from "../../components/UserModal";
+import { useModalContext } from "../../contexts/ModalContext";
 
 const Home = () => {
+	const { modalConfig } = useModalContext();
 	return (
 		<div>
 			<h1>List</h1>
 			<UserList />
+			{modalConfig.isModalOpen && <UserModal />}
 		</div>
 	);
 };
